9aaea89cb4f8e12d3ebb53bf6e2cf94e2df148e6
Richard Muchira <rwachira.rw@gmail.com>
2023-06-08 18:09:35 +0000

    added the __name__="__main__" guard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 
-for i in range(1, len(argv)):
-    print("{:d}: {:s}".format(i, argv[i]))
+
+def main():
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()
