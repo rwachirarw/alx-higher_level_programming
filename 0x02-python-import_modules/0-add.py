b70d225b7015e2bdb6028dc21b238f01f2a8f9fa
Richard Muchira <rwachira.rw@gmail.com>
2023-06-08 16:26:19 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+import add_0 as add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a,b)),  end=' ')
+print("{:d} + {:d} = {:d}".format(a, b, add.add(a,b)),  end=' ')
 print()
