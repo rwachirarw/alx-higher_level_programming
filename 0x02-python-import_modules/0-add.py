e6e83d97af4dd5f3ae84e598bb24d8428e5643db
Richard Muchira <rwachira.rw@gmail.com>
2023-06-08 16:43:42 +0000

    introduced if __name__=="__main__"

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
-add = add_0.add
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)),  end=' ')
-print()
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)),  end=' ')
+    print()
