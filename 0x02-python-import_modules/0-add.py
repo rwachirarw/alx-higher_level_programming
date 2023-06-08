f04cba9412e6dba4c3395d9926807acde23095dc
Richard Muchira <rwachira.rw@gmail.com>
2023-06-08 16:32:15 +0000

    Removed the as keyword when importing

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0 as add
+import add_0
+add = add_0.add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add.add(a, b)),  end=' ')
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)),  end=' ')
 print()
