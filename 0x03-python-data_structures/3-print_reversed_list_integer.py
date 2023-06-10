efd1fc57aa50d101655bf8179f346c57620b80ca
Richard Muchira <rwachira.rw@gmail.com>
2023-06-10 22:07:27 +0000

    used while loop and the return statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list:
+    while my_list:
         for i in reversed(my_list):
             print("{:d}".format(i))
-    else:
-        print('None')
+        return
+    print('None')
