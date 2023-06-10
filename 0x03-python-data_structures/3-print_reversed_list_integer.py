b511ca7b61b6d28153318ad901770f47ce2f50cd
Richard Muchira <rwachira.rw@gmail.com>
2023-06-10 11:55:51 +0000

    3-print_reversed_list_integer.py: used a slice in the list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    reversed_str = list(reversed(my_list))
-    for i in range(0, len(reversed_str)):
-        print("{:d}".format(reversed_str[i]))
+    for i in my_list[::-1]:
+        print("{:d}".format(i))
