1338c87a3ebe969385066288b8a4fc16532d5042
Richard Muchira <rwachira.rw@gmail.com>
2023-06-10 21:24:39 +0000

    checked for an empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,4 +6,4 @@ def print_reversed_list_integer(my_list=[]):
         print('None')
     else:
         for i in reversed(my_list):
-            print("{}".format(i))
+            print("{:d}".format(i))
