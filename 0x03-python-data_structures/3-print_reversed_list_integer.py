cb39c6e59d6a88f16617e0867af5161b57c8d3d8
Richard Muchira <rwachira.rw@gmail.com>
2023-06-11 07:55:16 +0000

    Used if my_list condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    while my_list:
+    if my_list:
         for i in reversed(my_list):
             print("{:d}".format(i))
-        return
-    print('None')
