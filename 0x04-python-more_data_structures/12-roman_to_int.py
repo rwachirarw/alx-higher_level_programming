5310ea01ff14a9999f1476411abad30ebda16eec
Richard Muchira <rwachira.rw@gmail.com>
2023-06-14 18:46:44 +0000

    12-roman_to_int.py: pycodestyle fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    def roman_to_int(roman_string):
-        if not isinstance(roman_string, str):
-            return 0
+    if not isinstance(roman_string, str):
+        return 0
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
-    result = 0
+    number = 0
     prev_value = 0
     for char in roman_string[::-1]:
         value = roman_dict[char]
         if value < prev_value:
-            result -= value
+            number -= value
         else:
-            result += value
+            number += value
         prev_value = value
-    return result
+    return number
