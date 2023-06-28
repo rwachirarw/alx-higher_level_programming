7e8f2d6e28d38779b8d6a2da35ad2b0204b9f407
Richard Muchira <rwachira.rw@gmail.com>
2023-06-28 00:42:24 +0000

    102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+""" Defination of Class square """
+
+
+class Square:
+    """defines the class square """
+
+    def __init__(self, size=0):
+        """ initialize the object instance of the class
+        Args:
+            size: size of the square
+        """
+        self.__size = 0
+        self.size = size
+
+    @property
+    def size(self):
+        """gets the size of the square """
+        return self.__size
+
+    @size.setter
+    """ sets the size of the square """
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ returns the area of the square """
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """ compares the =="""
+        if isinstance(other, Square):
+            return self.area() == other.area()
+        return False
+
+    def __ne__(self, other):
+        """ compares th !="""
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        """ compares the < """
+        if isinstance(other, Square):
+            return self.area() < other.area()
+        return False
+
+    def __le__(self, other):
+        """compares the <="""
+        if isinstance(other, Square):
+            return self.area() <= other.area()
+        return False
+
+    def __gt__(self, other):
+        """ compares the > """
+        if isinstance(other, Square):
+            return self.area() > other.area()
+        return False
+
+    def __ge__(self, other):
+        """ compares the >= """
+        if isinstance(other, Square):
+            return self.area() >= other.area()
+        return False
