bbc818e90dfa6ba106e8a444fb662febf6af73da
Richard Muchira <rwachira.rw@gmail.com>
2023-07-04 01:38:12 +0300

    9-rectangle.py: changed the order of initialization

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         ''' Initialises instance of a class'''
-        self.height = height
         self.width = width
+        self.height = height
         type(self).number_of_instances += 1
 
     @property
