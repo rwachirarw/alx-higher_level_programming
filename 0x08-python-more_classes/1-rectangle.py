f0b6fc2bd19ea9d67e51175654e33109e6bae11c
Richard Muchira <rwachira.rw@gmail.com>
2023-07-03 23:24:29 +0300

    1-rectangle.py: changed the order of initialization

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     '''Defines a Rectangle'''
     def __init__(self, width=0, height=0):
         ''' Initialises instance of a class'''
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
