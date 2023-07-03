4154fda990c53fa22ce109724cdb4e516591407d
Richard Muchira <rwachira.rw@gmail.com>
2023-07-03 21:07:46 +0300

    0x08-python-more_classes

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    '''
+    This function that adds two integers.
+
+    >>> add_integer(2, 3)
+    5
+
+    >>> add_integer(2.5, 3.5)
+    5
+
+    >>> add_integer('2', '3')
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
+
+    >>> add_integer(2, '3')
+    Traceback (most recent call last):
+        ...
+    TypeError: b must be an integer
+    '''
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
