819c5547d7bf0a323d0135f3b6618434235808fa
Richard Muchira <rwachira.rw@gmail.com>
2023-07-03 23:37:18 +0300

    2-rectangle.py: removed white spaces

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,9 +40,9 @@ class Rectangle:
     def area(self):
         '''Returns the area of the rectangle'''
         return self.__width * self.__height
-    
+
     def perimeter(self):
-        '''Returns the preimeter of the rectangle'''
+        '''Returns the perimeter of the rectangle'''
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__height * 2) + (self.__width * 2)
