5a08ed105c3659cb3d994e9e96ae4e1b025e2f88
Richard Muchira <rwachira.rw@gmail.com>
2023-07-04 18:42:47 +0300

    Added the docstrings

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
+"""prevents the user from dynamically \
+   creating new instance attributes, \
+   except if the new instance attribute is called first_name
+
+Raises:
+    AttributeError: 'LockedClass' object has \
+                             no attribute '
+"""
 
 
 class LockedClass:
     __slots__ = ['first_name']
+    '''Class definition'''
 
     def __init__(self):
+        '''Initializes instance of a class'''
         self.first_name = None
 
     def __setattr__(self, name, value):
+        '''Sets atrributes for the instance'''
         if name != 'first_name':
             raise AttributeError("'LockedClass' object has \
                                  no attribute '" + name + "'")
