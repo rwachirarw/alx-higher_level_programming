748413d3803f9482e156c8396d60eb7369212966
Richard Muchira <rwachira.rw@gmail.com>
2023-07-11 22:44:32 +0300

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Write a function that returns the dictionary \
+    description with simple data structure \
+        (list, dictionary, string, integer and\
+              boolean) for JSON serialization of an object:
+"""
+
+
+def class_to_json(obj):
+    """
+
+    Args:
+        obj (string): JSON object
+
+    Returns:
+        dict: dict object
+    """
+    return obj.__dict__
