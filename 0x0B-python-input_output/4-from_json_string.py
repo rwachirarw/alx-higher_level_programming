c7bb48c32a73a7a05ab455d7ad755422246c7663
Richard Muchira <rwachira.rw@gmail.com>
2023-07-11 21:49:35 +0300

    Added a docstring before import for 4 and 5

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import json
 """
 Write a function that returns an object (Python data structure) \
     represented by a JSON string:
 """
+import json
 
 
 def from_json_string(my_str):
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import json
 """
 Write a function that writes an Object to a text file, \
     using a JSON representation:
 """
+import json
 
 
 def save_to_json_file(my_obj, filename):
