c46dd3f13784a20c2cac2449be463149367ac63a
Richard Muchira <rwachira.rw@gmail.com>
2023-07-11 22:50:06 +0300

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by:
+
+    Public instance attributes:
+        first_name
+        last_name
+        age
+"""
+
+
+def __init__(self, first_name, last_name, age):
+    """
+    initialization of instance
+
+    Args:
+        first_name (str): first name
+        last_name (str): last name
+        age (int): age
+    """
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+
+def to_json(self):
+    """
+
+    Returns:
+        dict: dictionary object
+    """
+    return self.__dict__
