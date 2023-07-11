3f474e0c1ed0925a0033b6377f361ce1dc2f3dd5
Richard Muchira <rwachira.rw@gmail.com>
2023-07-11 17:23:36 +0300

    #!/python3 correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,5 @@ def read_file(filename=""):
     Args:
         filename (str, optional): file name to be read. Defaults to "".
     """
-    with open(filename) as my_file:
+    with open(filename, "r") as my_file:
         print(my_file.read())
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """
 A method to write to a file
 """
