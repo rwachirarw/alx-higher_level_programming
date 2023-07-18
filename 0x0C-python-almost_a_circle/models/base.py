bc1c71cfe60d4dab799a80d958e941b0f91e51ba
Richard Muchira <rwachira.rw@gmail.com>
2023-07-18 04:58:44 +0300

    base update to handle csv

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ Base class
 """
 import json
 import csv
+import turtle
 
 
 class Base:
@@ -106,74 +107,73 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """Serialize instances to a CSV file
+        """Serializes list_objs in CSV format and saves it to a file.
 
         Args:
-            list_objs (list): List of instances
-
-        Returns:
-            None
+            - list_objs: list of instances
         """
-        filename = cls.__name__ + ".csv"
-        fieldnames = cls.get_csv_fieldnames()
 
-        with open(filename, "w", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            for obj in list_objs:
-                writer.writerow(obj.to_csv_row())
+        if (
+            type(list_objs) != list
+            or (list_objs is not None and not
+                all(isinstance(x, cls) for x in list_objs))
+        ):
+            raise TypeError("list_objs must be a list of instances")
+
+        filename = cls.__name__ + ".csv"
+        with open(filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            if list_objs is not None:
+                if cls.__name__ == 'Rectangle':
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == 'Square':
+                    fields = ['id', 'size', 'x', 'y']
+                writer.writerow(fields)
+                for obj in list_objs:
+                    row = [getattr(obj, field) for field in fields]
+                    writer.writerow(row)
 
     @classmethod
     def load_from_file_csv(cls):
-        """Deserialize instances from a CSV file
+        """Deserializes CSV format from a file.
 
-        Returns:
-            list: List of instances deserialized from the CSV file
+        Returns: list of instances
         """
+
         filename = cls.__name__ + ".csv"
         instances = []
-
-        with open(filename, "r", newline="") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                instance = cls.create(**cls.from_csv_row(row))
-                instances.append(instance)
-
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                reader = csv.reader(f)
+                fields = next(reader)
+                for row in reader:
+                    instance = cls.create(**dict(zip(fields, row)))
+                    instances.append(instance)
         return instances
 
-    def to_dictionary(self):
-        """Return the dictionary representation of the instance
-
-        Returns:
-            dict: Dictionary representation of the instance
-        """
-        raise NotImplementedError
-
-    def to_csv_row(self):
-        """Return the CSV row representation of the instance
-
-        Returns:
-            dict: Dictionary representing a row in the CSV file
-        """
-        raise NotImplementedError
-
     @classmethod
-    def get_csv_fieldnames(cls):
-        """Return the fieldnames for the CSV file
-
-        Returns:
-            list: List of fieldnames
-        """
-        raise NotImplementedError
-
-    @classmethod
-    def from_csv_row(cls, csv_row):
-        """Create and return a dictionary from a CSV row
+    def draw(cls, list_rectangles, list_squares):
+        """Opens a window and draws all the Rectangles and Squares.
 
         Args:
-            csv_row (dict): Dictionary representing a row in the CSV file
-
-        Returns:
-            dict: Dictionary containing attribute values
+            - list_rectangles: list of Rectangle instances
+            - list_squares: list of Square instances
         """
-        raise NotImplementedError
+        window = turtle.Screen()
+        pen = turtle.Pen()
+        figures = list_rectangles + list_squares
+
+        for fig in figures:
+            pen.up()
+            pen.goto(fig.x, fig.y)
+            pen.down()
+            pen.forward(fig.width)
+            pen.right(90)
+            pen.forward(fig.height)
+            pen.right(90)
+            pen.forward(fig.width)
+            pen.right(90)
+            pen.forward(fig.height)
+            pen.right(90)
+
+        window.exitonclick()
