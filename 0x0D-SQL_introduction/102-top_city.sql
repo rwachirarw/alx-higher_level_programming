4da666e3fe53195f4579bb181293a888ff9f175c
Richard Muchira <37326416+rwachirarw@users.noreply.github.com>
2023-10-17 21:03:39 +0300

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/102-top_city.sql
+++
@@ -1,2 +0,0 @@
--- a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-SELECT `city`, AVG(`value`) 'avg_temp' FROM `temperatures` WHERE `month` = 8 OR `month` = 7 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
